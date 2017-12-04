2ac083dac1d939d85f58166d22d1c80a5d2dcbef
Melissa Bell <melissabell456@gmail.com>
2017-12-04 08:57:04 -0600

    completed MVP

--- chaining.js
+++ chaining.js
@@ -1,11 +1,5 @@
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-var newIntegers = integers.sort(function(first, second) {return second-first}).filter(function(x) {return x < 19}).map(function(x) {return x * 1.5 - 1});
+var newIntegers = integers.sort(function(first, second) {return second-first}).filter(function(x) {return x < 19}).map(function(x) {return x * 1.5 - 1}).reduce(function(a, b){return a + b});
 
 console.log(newIntegers);
-
-
-
-
-// Then output (either in the DOM or the console) the sum of all the resulting numbers.
-
